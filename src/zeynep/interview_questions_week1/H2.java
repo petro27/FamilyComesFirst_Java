82a0ae589b55cd15326b1a5ab73bc56a9158f9e3
ZEYNEP <zeynepkina91@gmail.com>
2022-05-25 14:23:22 -0400

    I added week1 interview questions(from mentor)

--- src/zeynep/interview_questions_week1/H2.java
+++ src/zeynep/interview_questions_week1/H2.java
@@ -19,6 +19,6 @@ public class H2 {
     }
 
     public static void main(String[] args) {
-        System.out.println(sameLetters("abc", "bhb"));
+        System.out.println(sameLetters("abc", "bhp"));
     }
 }
