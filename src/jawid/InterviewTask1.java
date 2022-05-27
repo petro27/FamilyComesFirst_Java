f33d6d1dc6ce660d59855fb29b08f3265ccd41f9
jawid <jawidahmada4@gmail.com>
2022-05-26 21:15:40 -0400

    Task2,3,4,5

--- src/jawid/InterviewTask1.java
+++ src/jawid/InterviewTask1.java
@@ -1,5 +1,9 @@
 package jawid;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class InterviewTask1 {
     /*
 1️⃣ String - Sort Letters and Numbers from alphanumeric String
@@ -11,7 +15,29 @@ OutPut: "CD015ACCCG011899"
      */
     public static void main(String[] args) {
         String str = "DC501GCCCA098911";
+        ArrayList<Character> letters = new ArrayList<>();
+        ArrayList<Character> digits = new ArrayList<>();
+        ArrayList<Character> lAndDTogether = new ArrayList<>();
 
+        for (int i = 0; i < str.length(); i++) {
 
+            if (Character.isAlphabetic(str.charAt(i))) {
+                lAndDTogether.addAll(digits);
+                digits.clear();
+                letters.add(str.charAt(i));
+                Collections.sort(letters);
+            } else if (Character.isDigit(str.charAt(i))) {
+                lAndDTogether.addAll(letters);
+                letters.clear();
+                digits.add(str.charAt(i));
+                Collections.sort(digits);
+            }
+        }
+       if (letters.isEmpty() ? lAndDTogether.addAll(digits) : lAndDTogether.addAll(letters));
+        System.out.println(lAndDTogether.toString()
+                .replace(", ", "")
+                .replace("[", "")
+                .replace("]", ""));
     }
-}
+    }
+
