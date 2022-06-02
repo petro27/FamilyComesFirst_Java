7563f1c7a02950ead5cd4eae8e59da168615fa95
jawid <jawidahmada4@gmail.com>
2022-06-02 17:17:29 -0400

    Included fori loop inside if statement and it works

--- src/jawid/week3/ArrayNUniqueInteger.java
+++ src/jawid/week3/ArrayNUniqueInteger.java
@@ -9,18 +9,19 @@ Write a function:
 that, given an integer N (1 < N < 100), returns an array containing N unique integers that sum up to 0. The function can return any such array. For example, given N = 4, the function could return [1,0, -3,2] or [-2,1, -4,5]. The answer [1, -1,1,3] would be incorrect (because value 1 occurs twice). For N = 3 one of the possible answers is [-1,0,1]  (but there are many more correct answers).
 */
     public static int [] sumZero(int n){
-        int[] sumofZero = new int[n];
+        int[] sumUpt0Zero = new int[n];
         int index = 0;
         if (n % 2 == 1) {
-            sumofZero[index++] = 0;
+
+            sumUpt0Zero[index++] = 0;
             n /= 2;
             for (int i = 1; i <= n; i++) {
-                sumofZero[index++] = i;
-                sumofZero[index++] = -i;
+                sumUpt0Zero[index++] = i;
+                sumUpt0Zero[index++] = -i;
             }
         }
-        Arrays.sort(sumofZero);
-        return sumofZero;
+        Arrays.sort(sumUpt0Zero);
+        return sumUpt0Zero;
     }
 
     public static void main(String[] args) {
