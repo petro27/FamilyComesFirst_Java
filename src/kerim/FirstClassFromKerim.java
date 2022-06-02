d208a152762ffe59cba9c9fd44bb19e8a0ca5f5c
petro <petrokozovyi@gmail.com>
2022-06-01 21:03:20 -0500

    SortDescending task

--- src/kerim/FirstClassFromKerim.java
+++
@@ -1,8 +0,0 @@
-package kerim;
-
-public class FirstClassFromKerim {
-
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
--- src/zeynep/zeynep.java
+++
@@ -1,6 +0,0 @@
-package zeynep;
-
-public class zeynep {
-
-    // I added something
-}
