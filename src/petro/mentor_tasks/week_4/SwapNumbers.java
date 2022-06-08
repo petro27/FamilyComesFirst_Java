5bcb33e03812e93a39dc48121864cd724f0f90d2
petro <petrokozovyi@gmail.com>
2022-06-08 18:14:03 -0500

    week4

---
+++ src/petro/mentor_tasks/week_4/SwapNumbers.java
@@ -0,0 +1,23 @@
+package petro.mentor_tasks.week_4;
+
+import java.util.*;
+
+public class SwapNumbers {
+    //Numbers - Swap Numbers
+
+    public static void swapNumbers(List<Integer>list) {
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        for (int i = 0; i < list.size(); i += 2) {
+
+            list1.add(list.get(i + 1));
+            list1.add(list.get(i));
+
+        }
+        System.out.println(list1);
+    }
+
+    public static void main(String[] args) {
+        swapNumbers(Arrays.asList(1,2,3,4,5,6));
+    }
+}
