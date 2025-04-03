85f01148c202c3552a92a607de0cf5d5ffc52d7f
magymekaeel <magy.usama@yahoo.com>
2025-04-03 16:50:52 -0400

    Example about fizz buzz

--- src/Magy/MK001.java
+++ src/Magy/MK001.java
@@ -10,6 +10,18 @@ public class MK001 {
           Divisibility by 5: If a number is divisible by 5, print "Buzz" instead of the number.
           Divisibility by both 3 and 5: If a number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
           Example:  */
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a number to find the divisibility");
+        int num = input.nextInt();
 
+        if(num % 3 == 0 && num % 5 == 0){
+            System.out.println("fizz buzz");
+        } else if (num % 3 == 0) {
+            System.out.println("fizz");
+        } else if (num % 5 == 0) {
+            System.out.println("buzz");
+        }else {
+            System.out.println("that number is nut evenly divisible");
+        }
     }
 }
