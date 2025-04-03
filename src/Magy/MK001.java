636903cbf6cb0589fde6d2389173659b822f3962
magymekaeel <magy.usama@yahoo.com>
2025-04-03 16:50:08 -0400

    task to remind you about if statement

---
+++ src/Magy/MK001.java
@@ -0,0 +1,15 @@
+package Magy;
+
+import java.util.Scanner;
+
+public class MK001 {
+    public static void main(String[] args) {
+
+        /*Print Numbers 1 to 100: The program should iterate through numbers from 1 to 100, inclusive.
+          Divisibility by 3: If a number is divisible by 3, print "Fizz" instead of the number.
+          Divisibility by 5: If a number is divisible by 5, print "Buzz" instead of the number.
+          Divisibility by both 3 and 5: If a number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
+          Example:  */
+
+    }
+}
